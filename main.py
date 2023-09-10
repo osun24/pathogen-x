9df80bb54d3272038ddac9f59d03b3ac6e5b0834
osun24 <osun342@gmail.com>
2023-09-10 16:52:23 -0700

    add pie chart

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Status(Enum):
 
 # Agent - an individual "person" in the simulation
 class Agent:
+    """ An agent in the simulation with a status, position, and sociability """
     def __init__(self, status, x0, y0, sociability=0.5):
         self.status = status
         self.x = x0
@@ -26,35 +27,38 @@ class Agent:
 class SIRSimulatorUI:
     def __init__(self, root, total_population=1000, initial_infectious=1, beta=1, num_time_steps=1000, exposure_distance=50):
         self.root = root
-        self.root.geometry("800x800")  # Width x Height
+        self.root.geometry("1400x900")  # Width x Height
         self.root.title("Agent-Based SIR Model Simulator - Ball")
         
         # Main frame to hold all other frames
         self.main_frame = ttk.Frame(root)
-        self.main_frame.pack(side=tk.LEFT, padx=20, pady=20, fill=tk.BOTH, expand=True)
-
+        self.main_frame.pack(side=tk.LEFT, padx=0, pady=0, fill=tk.BOTH, expand=True)
+        
         # Frame for Tkinter widgets
         self.frame = ttk.Frame(self.main_frame)
-        self.frame.grid(row=2, column=0, sticky='nsew')
-
-        # Canvas for simulation
-        self.canvas = tk.Canvas(self.main_frame, width=800, height=400, bg='white')
-        self.canvas.grid(row=1, column=0, sticky='nsew')
-
-        # Frame for Matplotlib graphs
-        self.fig, self.ax = plt.subplots(figsize=(8, 4))
-        self.canvas_fig = FigureCanvasTkAgg(self.fig, master=self.main_frame)
-        self.canvas_fig.get_tk_widget().grid(row=0, column=0, sticky='nsew')
         
         # Checkbox for social distancing
         self.social_distancing = tk.BooleanVar()
         self.check_button = tk.Checkbutton(self.frame, text="Social Distancing", variable=self.social_distancing)
         self.check_button.pack()
+        
+        self.frame.grid(row=0, column=0, sticky='nsew')
+
+        # Canvas for simulation
+        self.canvas = tk.Canvas(self.main_frame, width=800, height=800, bg='white')
+        self.canvas.grid(row=1, column=0, sticky='nsew')
+
+        # Frame for Matplotlib graphs: 1x2 grid of subplots - one for SIR model, one for pie chart
+        self.fig, (self.ax, self.pie_ax) = plt.subplots(2, 1, figsize=(3, 7))
+        self.fig.subplots_adjust(hspace=0.75)  # Adjust the space between subplots
+        self.canvas_fig = FigureCanvasTkAgg(self.fig, master=self.main_frame)
+        self.canvas_fig.get_tk_widget().grid(row=1, column=1, sticky='nsew')
 
         # Configure the grid system
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.main_frame.grid_columnconfigure(1, weight=1)
         self.main_frame.grid_rowconfigure(0, weight=1)
-        self.main_frame.grid_rowconfigure(1, weight=1)
-        self.main_frame.grid_rowconfigure(2, weight=1)
+        self.main_frame.grid_rowconfigure(1, weight=8)
         
         # Generate random positions for agents
         self.positions = np.random.randint(0, 800, size=(total_population, 2))
@@ -66,10 +70,11 @@ class SIRSimulatorUI:
         self.num_time_steps = num_time_steps
         self.current_step = 0
         self.exposure_distance = exposure_distance
-        self.history = []
+        self.history = np.array([(total_population - initial_infectious), initial_infectious, 0]) 
         self.root.after(100, self.run_simulation)
 
     def agent_color(self, agent):
+        """ Return the color of the agent based on its status """
         if agent.status == Status.SUSCEPTIBLE:
             return 'blue'
         elif agent.status == Status.INFECTED:
@@ -94,12 +99,14 @@ class SIRSimulatorUI:
             return 'green'
 
     def update_ui(self):
+        """ Update the UI with the current agent positions and status counts """
         if self.current_step % 2 != 0:  # Update UI every 2 steps
             return
         self.canvas.delete("all")
         
         if self.current_step % 10 == 0:  # Update graphs every 10 steps
-            self.show_graphs() 
+            self.show_sir() 
+            self.update_pie_chart()
 
         counts = {'S': 0, 'I': 0, 'R': 0}
         
@@ -119,6 +126,7 @@ class SIRSimulatorUI:
             self.canvas.create_text(10, canvas_height - (25 - idx * 10), text=f"{status}: {counts[status]}", fill=color, anchor='sw')
 
     def run_simulation(self):
+        """ Run the simulation for one time step """
         if self.current_step >= self.num_time_steps: 
             return
 
@@ -127,7 +135,7 @@ class SIRSimulatorUI:
         agent_statuses = np.array([agent.status for agent in self.agents])
         infectious_agents = agent_statuses == Status.INFECTED
         susceptible_agents = agent_statuses == Status.SUSCEPTIBLE
-        self.history.append((np.sum(susceptible_agents), np.sum(infectious_agents), np.sum(agent_statuses == Status.RECOVERED)))
+        self.history = np.vstack((self.history, [np.sum(susceptible_agents), np.sum(infectious_agents), np.sum(agent_statuses == Status.RECOVERED)]))
 
         # Calculate distances between each pair of agents using a KD-tree
         tree = KDTree(agent_positions)
@@ -157,17 +165,34 @@ class SIRSimulatorUI:
         self.current_step += 1
         self.root.after(40, self.run_simulation)
 
-    def show_graphs(self):
-        # Create SIR model plot with matplotlib
+    def show_sir(self):
+        """ Create SIR model plot with matplotlib """
         self.ax.clear()
         history_arr = np.array(self.history)
         self.ax.plot(history_arr[:, 0], label='Susceptible')
         self.ax.plot(history_arr[:, 1], label='Infected')
         self.ax.plot(history_arr[:, 2], label='Recovered')
         self.ax.legend()
-        self.ax.set_title("SIR Model")
+        self.ax.set_title("History")
         self.ax.set_xlabel("Time")
         self.ax.set_ylabel("Population")
+        self.canvas_fig.draw()
+    
+    def update_pie_chart(self):
+        """Update the pie chart based on the current status counts."""
+        self.pie_ax.clear()
+
+        history_arr = np.array(self.history)
+        counts = {'S': history_arr[-1, 0], 'I': history_arr[-1, 1], 'R': history_arr[-1, 2]}
+        
+        # Labels for the sections of our pie chart
+        labels = ['Susceptible', 'Infected', 'Recovered']
+        sizes = [counts['S'], counts['I'], counts['R']]
+        
+        self.pie_ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        self.pie_ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        self.pie_ax.set_title("Current Distribution")
         self.canvas_fig.draw()
 
 if __name__ == "__main__":
